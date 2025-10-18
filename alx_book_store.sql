bfca645e1ed5bdebedc8b3ef5d66cd0d3e607f13
Your Name <josephsegzy@gmail.com>
2025-10-18 17:52:20 +0100

    Fix database schema to pass automated checks

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,47 +1,57 @@
+-- Drop database if exists
 DROP DATABASE IF EXISTS alx_book_store;
-CREATE DATABASE alx_book_store
+
+-- Create database
+CREATE DATABASE IF NOT EXISTS alx_book_store
 DEFAULT CHARACTER SET = utf8mb4
 DEFAULT COLLATE = utf8mb4_unicode_ci;
+
+-- Use database
 USE alx_book_store;
 
-CREATE TABLE Authors (
-  author_id INT NOT NULL AUTO_INCREMENT,
-  author_name VARCHAR(215) NOT NULL,
-  PRIMARY KEY (author_id)
+-- Authors table
+CREATE TABLE IF NOT EXISTS Authors (
+    author_id INT NOT NULL AUTO_INCREMENT,
+    author_name VARCHAR(215) NOT NULL,
+    PRIMARY KEY (author_id)
 );
 
-CREATE TABLE Books (
-  book_id INT NOT NULL AUTO_INCREMENT,
-  title VARCHAR(130) NOT NULL,
-  author_id INT NOT NULL,
-  price DOUBLE NOT NULL DEFAULT 0.0,
-  publication_date DATE NULL,
-  PRIMARY KEY (book_id),
-  FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+-- Books table
+CREATE TABLE IF NOT EXISTS Books (
+    book_id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(130) NOT NULL,
+    author_id INT NOT NULL,
+    price DOUBLE NOT NULL DEFAULT 0.0,
+    publication_date DATE,
+    PRIMARY KEY (book_id),
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
-CREATE TABLE Customers (
-  customer_id INT NOT NULL AUTO_INCREMENT,
-  customer_name VARCHAR(215) NOT NULL,
-  email VARCHAR(215) NOT NULL,
-  address TEXT,
-  PRIMARY KEY (customer_id)
+-- Customers table
+CREATE TABLE IF NOT EXISTS Customers (
+    customer_id INT NOT NULL AUTO_INCREMENT,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) NOT NULL,
+    address TEXT,
+    PRIMARY KEY (customer_id)
 );
 
-CREATE TABLE Orders (
-  order_id INT NOT NULL AUTO_INCREMENT,
-  customer_id INT NOT NULL,
-  order_date DATE NOT NULL,
-  PRIMARY KEY (order_id),
-  FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+-- Orders table
+CREATE TABLE IF NOT EXISTS Orders (
+    order_id INT NOT NULL AUTO_INCREMENT,
+    customer_id INT NOT NULL,
+    order_date DATE NOT NULL,
+    PRIMARY KEY (order_id),
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
-CREATE TABLE Order_Details (
-  orderdetailid INT NOT NULL AUTO_INCREMENT,
-  order_id INT NOT NULL,
-  book_id INT NOT NULL,
-  quantity DOUBLE NOT NULL DEFAULT 1.0,
-  PRIMARY KEY (orderdetailid),
-  FOREIGN KEY (order_id) REFERENCES Orders(order_id),
-  FOREIGN KEY (book_id) REFERENCES Books(book_id)
+-- Order_Details table
+CREATE TABLE IF NOT EXISTS Order_Details (
+    orderdetailid INT NOT NULL AUTO_INCREMENT,
+    order_id INT NOT NULL,
+    book_id INT NOT NULL,
+    quantity DOUBLE NOT NULL DEFAULT 1.0,
+    PRIMARY KEY (orderdetailid),
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
